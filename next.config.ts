d8d0ac706ceebc426ee939036e9c4d2c8bd568a9
Nir Maman <9172512400n@users.noreply.github.com>
2026-05-29 19:33:32 -0400

    Redirect old FindYourTow URL to RoadAssistNow

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "findyourtow.vercel.app",
+          },
+        ],
+        destination: "https://roadassistnow.vercel.app/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
